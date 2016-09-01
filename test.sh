26fb98ead9da886028d11d22b7633cd8e356f07a
root <redchen1255@gmail.com>
2016-09-01 17:19:29 +0800

    update

--- test.sh
+++ test.sh
@@ -2,8 +2,14 @@
 
 file_list=`find bin -name "*_unittest"`
 
+flag=0
+
 for file_path in $file_list
 do 
     $file_path
-    [ $? != 0 ] && echo "Test fail" && exit 1
+    [ $? != 0 ] && flag=1
 done
+
+[ $flag != 0 ] && echo "test fail."
+
+exit $flag
