b9eaa98aecca5463e60c19a8f57ee74003dbad07
redchen1255 <redchen1255@gmail.com>
2017-02-04 05:47:20 -0500

    up

--- sample/logging/logging_test.c
+++ sample/logging/logging_test.c
@@ -5,13 +5,18 @@
 #include <string.h>
 #include <unistd.h>
 
-static Logger* log = NULL;
+static Logger* log_a0 = NULL;
+static Logger* log_a1 = NULL;
 
 void test(int i) {
-    LOG_DEBUG(log, "%d", i);
-    LOG_INFO(log, "%d", i);
-    LOG_ERROR(log, "%d", i);
-    LOG_FATAL(log, "%d", i);
+    LOG_DEBUG(log_a0, "%d", i);
+    LOG_INFO(log_a0, "%d", i);
+    LOG_ERROR(log_a0, "%d", i);
+    LOG_FATAL(log_a0, "%d", i);
+    LOG_DEBUG(log_a1, "%d", i);
+    LOG_INFO(log_a1, "%d", i);
+    LOG_ERROR(log_a1, "%d", i);
+    LOG_FATAL(log_a1, "%d", i);
 }
 
 int main() {
@@ -20,13 +25,15 @@ int main() {
     LOGGING_INIT("logging.conf");
     //LOGGING_INIT(NULL);
 
-    log = LOGGING_GET_LOGGER("/test/A0/example");
+    log_a0 = LOGGING_GET_LOGGER("/test/A0/example");
+    log_a1 = LOGGING_GET_LOGGER("/test/A1/example");
 
     for (int i = 0; i < 100; i++) {
         test(i);
     }
 
-    LOGGER_FLUSH(log);
+    LOGGER_FLUSH(log_a0);
+    LOGGER_FLUSH(log_a1);
     puts("end !");
     sleep(1);
     return 0;
